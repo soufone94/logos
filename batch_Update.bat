233a1c9d299a1fd553b91c0fb11179d11ffafefc
soufon94 <soufone94@gmail.com>
2017-07-08 20:42:33 +0200

    update repo.

---
+++ batch_Update.bat
@@ -0,0 +1,15 @@
+@echo off
+
+title Update logo.
+
+set WorkingDir=%~dp0
+
+
+echo update soufone repo
+
+echo commit to github
+git config credential.helper store
+git config --global user.name "soufon94"
+git add *
+git commit -m "update repo."
+git push --set-upstream origin master
