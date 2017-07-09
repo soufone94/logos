7da18b9e37cca77dd707173d7e9d1f173b7ed67f
soufon94 <soufone94@gmail.com>
2017-07-09 17:29:58 +0200

    update repo.

--- batch_Update.bat
+++ batch_Update.bat
@@ -8,7 +8,7 @@ set WorkingDir=%~dp0
 echo update soufone repo
 
 echo commit to github
-git config core.ignorecase true
+git config core.ignorecase false
 git config credential.helper store
 git config --global user.name "soufon94"
 git add *
